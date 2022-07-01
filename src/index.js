b2683273c87d47733efd5589b530b79a7b9f71ae
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-01 15:39:48 -0700

    added functionality to show specific errpr messages based on inputs

--- src/index.js
+++ src/index.js
@@ -4,27 +4,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Rate from './js/rates.js';
 
-function getElements(amount, response) {
-  if (response.conversion_result) {
-    $('#test').text(`${amount} ${response.base_code} to ${response.target_code} is ${response.conversion_result}`);
-    
-  } else {
-    $('#test2').text(`There was an error: ${response}`);
-  }
-}
-
-async function makeApiCall(country1,country2,amount) {
-  const response = await Rate.getRate(country1,country2,amount);
-  getElements(amount,response);
-}
-
-
 
 $(document).ready(function() {
-  $('#btn').click(function() {
+    $('#btn').click(async function() {
+
     const country1 = $('#currency1').val();
     const country2 = $('#currency2').val();
     const amount = $('#amount').val();
-    makeApiCall(country1,country2, amount);
+
+    if (country1 === "ZB" || country2 === "ZB") {
+        $("#test").text(`Error: ZB is not a supported currency...yet`);
+        return 
+      } else if (!Number(amount)) {
+        $("#test").text(`Please enter a number!`);
+      } else if (Math.sign(amount) !== 1) {
+        $("#test").text(`Please enter a positive value!`);
+      } else {
+        let response = await Rate.getRate(country1,country2,amount);
+        if (response.conversion_result) {
+        $('#test').text(`${amount} ${response.base_code} to ${response.target_code} is ${response.conversion_result}`);
+        } else {
+        $('#test2').text(`There was an error: ${response}`);
+        } 
+      }
   });
 });
+//zz
