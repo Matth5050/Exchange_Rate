23f842954a7bc6997839ccef6fd624776d57f8ad
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-01 13:44:08 -0700

    WIP working on printing error message to html

--- src/js/rates.js
+++ src/js/rates.js
@@ -1,8 +1,9 @@
 export default class Rate {
-    static async getRate(country1,country2,amount) {
+    static async getRate(country1,country2, amount) {
       try {
         const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${country1}/${country2}/${amount}`);
         if (!response.ok) {
+          console.log(response.statusText);
           throw Error(response.statusText);
         }
         return response.json();
