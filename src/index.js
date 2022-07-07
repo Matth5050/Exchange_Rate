180c2cf14dee6650bf72358e2b9d986dc5acb50f
Matth5050 <matthew.f.herbert@gmail.com>
2022-07-06 19:59:42 -0700

    fixed indentation across multiple files

--- src/index.js
+++ src/index.js
@@ -6,61 +6,61 @@ import Rate from './js/rates.js';
 import Coin from './js/coin.js';
 
 function displayData(data) {
-    for (const property in data) {
-        $('.coinOut').append(`<p class="${property}"><b>${property}:<b></p>`);
-        Object.keys(data[property]).forEach(key => {
-            $('.coinOut').append(`<p class="${property}">${key}: ${data[property][key]}</p>`);
-        })
-    }
+  for (const property in data) {
+    $('.coinOut').append(`<p class="${property}"><b>${property}:<b></p>`);
+    Object.keys(data[property]).forEach(key => {
+        $('.coinOut').append(`<p class="${property}">${key}: ${data[property][key]}</p>`);
+    })
+  }
 }
 
 function getElements(data) {
-    if (data) {
-      displayData(data);
-    } else {
-      $('.coinOut').append(`<p>There was an error loading crypto data</p>`);
-    }
+  if (data) {
+    displayData(data);
+  } else {
+    $('.coinOut').append(`<p>There was an error loading crypto data</p>`);
+  }
 }
 
 async function coinPrice() {
-    const response = await Coin.getCoinRate();
-    return response; 
+  const response = await Coin.getCoinRate();
+  return response; 
 }
 
 function grabCoinPrice() {
-    let result = coinPrice();
-    result.then(function(data) {
-        getElements(data)
-    })
+  let result = coinPrice();
+  result.then(function(data) {
+      getElements(data)
+  })
 }
 
 $(document).ready(function() {
-    grabCoinPrice();
-    $('#btn').click(async function() {
-    const country1 = $('#currency1').val();
-    const country2 = $('#currency2').val();
-    const amount = $('#amount').val();
+  grabCoinPrice();
+  $('#btn').click(async function() {
+  const country1 = $('#currency1').val();
+  const country2 = $('#currency2').val();
+  const amount = $('#amount').val();
 
-    if (country1 === "ZB" || country2 === "ZB") {
-        $("#test").text(`Error: ZB is not a supported currency...yet`);
-        return 
-      } else if (!Number(amount)) {
-        $("#test").text(`Please enter a number!`);
-        return
-      } else if (Math.sign(amount) !== 1) {
-        $("#test").text(`Please enter a positive value!`);
-        return
-      } else {
-        let response = await Rate.getRate(country1,country2,amount);
-        sessionStorage.setItem(response,response.conversion_result);
-        let storage = sessionStorage.getItem(response);
-        if (response.conversion_result) {
-        $('#test').text(`${amount} ${country1} = ${storage} ${country2}`);
-        $('.output').slideDown();
-        } else {
-        $('#test2').text(`There was an error: ${response}`);
-        $('.output').slideDown();
-        } 
-      }
+  if (country1 === "ZB" || country2 === "ZB") {
+    $("#test").text(`Error: ZB is not a supported currency...yet`);
+    return 
+  } else if (!Number(amount)) {
+    $("#test").text(`Please enter a number!`);
+    return
+  } else if (Math.sign(amount) !== 1) {
+    $("#test").text(`Please enter a positive value!`);
+    return
+  } else {
+    let response = await Rate.getRate(country1,country2,amount);
+    sessionStorage.setItem(response,response.conversion_result);
+    let storage = sessionStorage.getItem(response);
+    if (response.conversion_result) {
+    $('#test').text(`${amount} ${country1} = ${storage} ${country2}`);
+    $('.output').slideDown();
+    } else {
+    $('#test2').text(`There was an error: ${response}`);
+    $('.output').slideDown();
+    } 
+  }
   });
 });
--- src/js/rates.js
+++ src/js/rates.js
@@ -1,13 +1,13 @@
 export default class Rate {
-    static async getRate(country1,country2, amount) {
-      try {
-        const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.CURRENCY_API_KEY}/pair/${country1}/${country2}/${amount}`);
-        if (!response.ok) {
-          throw Error(response.status);
-        }
-        return response.json();
-      } catch(error) {
-        return error.message;
+  static async getRate(country1,country2, amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.CURRENCY_API_KEY}/pair/${country1}/${country2}/${amount}`);
+      if (!response.ok) {
+        throw Error(response.status);
       }
+      return response.json();
+    } catch(error) {
+      return error.message;
     }
+  }
 }
